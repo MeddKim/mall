bbe284bb393ff9a5df9b16479eb7b56eb4ba9d6d
wang.zhongyi <meddkim@163.com>
2018-01-02 16:31:26 +0800

    add file

--- core-domain/src/test/java/com/doge/eight/five/Trader.java
+++ core-domain/src/test/java/com/doge/eight/five/Trader.java
@@ -1,8 +1,12 @@
 package com.doge.eight.five;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Trader {
     private String name;
     private String city;
