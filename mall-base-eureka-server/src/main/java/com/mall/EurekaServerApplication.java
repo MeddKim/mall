6ae86aa03bb4e82de35c975defc29b9089109964
unknown <meddkim@163.com>
2018-05-04 19:01:49 +0800

    fix

--- mall-base-eureka-server/src/main/java/com/mall/EurekaServerApplication.java
+++ mall-base-eureka-server/src/main/java/com/mall/EurekaServerApplication.java
@@ -1,11 +1,13 @@
 package com.mall;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
