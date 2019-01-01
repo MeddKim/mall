a63e145e0e875c37b27033145e999d7f09eac4f2
willard <meddkim@163.com>
2019-01-01 11:46:12 +0800

    fix

--- module-order-server/core-service/src/main/java/com/mall/order/interfaces/controller/AttributeController.java
+++ module-order-server/core-service/src/main/java/com/mall/order/interfaces/controller/AttributeController.java
@@ -1,20 +1,28 @@
 package com.mall.order.interfaces.controller;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mall.order.interfaces.client.AttributeService;
 import com.mall.order.interfaces.message.SinkSender;
 import com.mall.product.interfaces.exception.ValidateParamException;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/test")
+@Validated
 public class AttributeController {
 
     @Autowired
@@ -44,4 +52,28 @@ public class AttributeController {
     private Object msgTest(){
         return "成功";
     }
+
+
+    @PostMapping("model/test")
+    @Validated
+    public void modelTest(@RequestBody @Validated List<DemoReq> reqs, BindingResult result){
+        if(result.hasErrors()){
+            System.out.println(result.getFieldError().getDefaultMessage());
+            return;
+        }
+        System.out.println(reqs);
+    }
+    @Data
+    @Validated
+    private static class DemoReq{
+//        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+        private Date dateTime;
+        private Long id;
+        @NotNull(message = "不可为空")
+        private Integer age;
+        @NotBlank(message = "名称不可为空")
+        private String name;
+    }
+
 }
